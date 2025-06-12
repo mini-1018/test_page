4ba59024f181d24f5b6e3454553ea7634ba239bf
mini1018 <anjaemin1018@gmail.com>
2025-06-12 17:47:32 +0900

    [Feat] FWD 3줄로 변경

--- src/app/(black-nav)/products/food-waste-disposal/animationFWD.tsx
+++ src/app/(black-nav)/products/food-waste-disposal/animationFWD.tsx
@@ -72,6 +72,37 @@ export default function AnimationFWD() {
               />
             </div>
           </div>
+
+                    {/* 왼쪽으로 스크롤하는 첫 번째 이미지 그룹 */}
+                    <div className="flex carousell-scroll-left whitespace-nowrap">
+            <div className="flex-shrink-0 w-[1024px] lg:w-[1980px]">
+              <Image
+                src="/img/coda_fwd_01.webp"
+                width={1980}
+                height={400}
+                alt="스크롤 이미지"
+                className="w-full h-auto object-cover"
+              />
+            </div>
+            <div className="flex-shrink-0 w-[1024px] lg:w-[1980px]">
+              <Image
+                src="/img/coda_fwd_01.webp"
+                width={1980}
+                height={400}
+                alt="스크롤 이미지"
+                className="w-full h-auto object-cover"
+              />
+            </div>
+            <div className="flex-shrink-0 w-[1024px] lg:w-[1980px]">
+              <Image
+                src="/img/coda_fwd_01.webp"
+                width={1980}
+                height={400}
+                alt="스크롤 이미지"
+                className="w-full h-auto object-cover"
+              />
+            </div>
+          </div>
         </div>
       </section>
     </>
