42b45a3b855b5159999e956dadab7f64e4c2b2ce
mini1018 <anjaemin1018@gmail.com>
2025-06-12 16:51:51 +0900

    [Feat] FWD 사이즈 조정

--- src/app/(black-nav)/products/food-waste-disposal/animationFWD.tsx
+++ src/app/(black-nav)/products/food-waste-disposal/animationFWD.tsx
@@ -4,16 +4,16 @@ export default function AnimationFWD() {
   return (
     <>
       {/* 스크롤링 섹션 1 */}
-      <section className="w-full py-[75px] md:py-[150px] flex flex-col items-center">
+      <section className="w-full py-[75px] md:py-[150px] flex flex-col items-center mx-auto">
         <div className="text-center mb-12 px-4">
           <h2 className="font-bold text-third inline">
             전국 지자체가 선택한 검증된 음식물 종량기
           </h2>
         </div>
-        <div className="w-[80%] overflow-hidden relative">
+        <div className="w-full overflow-hidden relative">
           {/* 왼쪽으로 스크롤하는 첫 번째 이미지 그룹 */}
           <div className="flex carousell-scroll-left whitespace-nowrap">
-            <div className="flex min-w-full">
+            <div className="flex min-w-[1024px] w-[1024px] md:min-w-[1980px] md:w-[1980px]">
               <Image
                 src="https://do40f6yw4fd7i.cloudfront.net/img13/etc1.png"
                 width={1980}
@@ -22,7 +22,7 @@ export default function AnimationFWD() {
                 className="h-auto pt-16 inline-block"
               />
             </div>
-            <div className="flex min-w-full">
+            <div className="flex min-w-[1024px] w-[1024px] md:min-w-[1980px] md:w-[1980px]">
               <Image
                 src="https://do40f6yw4fd7i.cloudfront.net/img13/etc1.png"
                 width={1980}
@@ -34,7 +34,7 @@ export default function AnimationFWD() {
           </div>
           {/* 왼쪽으로 스크롤하는 첫 번째 이미지 그룹 */}
           <div className="flex carousell-scroll-right whitespace-nowrap">
-            <div className="flex min-w-full">
+            <div className="flex min-w-[1024px] w-[1024px] md:min-w-[1980px] md:w-[1980px]">
               <Image
                 src="https://do40f6yw4fd7i.cloudfront.net/img13/etc1.png"
                 width={1980}
@@ -43,7 +43,7 @@ export default function AnimationFWD() {
                 className="h-auto pt-16 inline-block"
               />
             </div>
-            <div className="flex min-w-full">
+            <div className="flex min-w-[1024px] w-[1024px] md:min-w-[1980px] md:w-[1980px]">
               <Image
                 src="https://do40f6yw4fd7i.cloudfront.net/img13/etc1.png"
                 width={1980}
